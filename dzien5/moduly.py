043ce594f331664afec9f233ea3d0237bf8868e8
Kamil <olejniczak.kam@gmail.com>
2017-07-28 17:13:54 +0200

    do ogarniecia moduly

---
+++ dzien5/moduly.py
@@ -0,0 +1,8 @@
+a = 1
+
+def info():
+    print("Jestem w funkcji info")
+    
+class Nowa:
+    def witaj():
+        print("tekst")
---
+++ dzien5/moduly2.py
@@ -0,0 +1,27 @@
+from moduly import *
+
+print(a)
+info()
+
+a = Nowa()
+a.witaj()
+'''
+moduly3
+
+print(F.closed, F.mode, F.name)
+
+F.write("Pocz¹tek pliku")
+F.write("Kolejny napis")
+F.writelines(["\n3 linia", "\n4 linia", "\n5 linia"])
+F.close()
+print(F.closed, F.mode, F.name)
+
+G = open(plik.txt","r")
+print(G.read(10))
+print(G.tell())
+G.seek(0)
+print(G.tell())
+G.seek(20)
+
+F.truncate
+'''
